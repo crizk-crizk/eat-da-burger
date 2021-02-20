de8774f63788700634f726993fe6facd7f47bddd
Carolina R <carolinarizk@gmail.com>
2021-02-20 16:55:16 -0500

    fixed event listeners for post and put methods

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,69 +1,42 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
   if (event) {
-    console.info('DOM loaded');
+    console.info("DOM loaded");
   }
 
   // UPDATE
-  const changeBurgerBtns = document.querySelectorAll('.change-burger');
+  const changeDevourBtns = document.querySelectorAll(".change-devour");
 
   // Set up the event listener for the create button
-  if (changeBurgerBtns) {
-    changeBurgerBtns.forEach((button) => {
-      button.addEventListener('click', (e) => {
-        console.log('test');
+  if (changeDevourBtns) {
+    changeDevourBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        console.log("test");
         // Grabs the id of the element that goes by the name, "id"
-        const id = e.target.getAttribute('data-id');
-        const newburger = e.target.getAttribute('data-newburger');
+        const id = e.target.getAttribute("data-id");
+        const newDevour = e.target.getAttribute("data-newdevour");
 
-        const newBurgerState = {
-          devoured: newburger,
+        const newDevourState = {
+          devoured: newDevour,
         };
 
         fetch(`/api/burgers/${id}`, {
-          method: 'PUT',
+          method: "PUT",
           headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+            Accept: "application/json",
+            "Content-Type": "application/json",
           },
 
           // make sure to serialize the JSON body
-          body: JSON.stringify(newBurgerState),
+          body: JSON.stringify(newDevourState),
         }).then((response) => {
           // Check that the response is all good
           // Reload the page so the user can see the new quote
           if (response.ok) {
-            console.log(`changed burger to: ${newBurger}`);
-            location.reload('/');
+            console.log(`changed burger to: ${newDevour}`);
+            location.reload("/");
           } else {
-            alert('something went wrong!');
-          }
-        });
-      });
-    });
-  }
-
-  // DELETE
-  const deleteBtns = document.querySelectorAll('.delete-burger');
-
-  // Set up the event listener for deleting a cat.
-  if (deleteBtns) {
-    deleteBtns.forEach((button) => {
-      button.addEventListener('click', (e) => {
-        // Grabs the id of the element that goes by the name, "id"
-        const id = e.target.getAttribute('data-id');
-        
-        fetch(`/api/burgers/${id}`, {
-          method: "DELETE"
-        })
-        .then((response) => {
-          // Check that the response is all good
-          // Reload the page so the user can see the new state
-          if (response.ok) {
-            console.log(`deleted burger: ${id}`);
-            location.reload('/');
-          } else {
-            alert('something went wrong!');
+            alert("something went wrong!");
           }
         });
       });
@@ -71,35 +44,60 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   // CREATE
-  const createBurgerBtn = document.getElementById('create-form');
+  const createBurgerBtn = document.getElementById("create-form");
 
   if (createBurgerBtn) {
-    createBurgerBtn.addEventListener('submit', (e) => {
+    createBurgerBtn.addEventListener("submit", (e) => {
       e.preventDefault();
-
       // Grabs the value of the textarea that goes by the name, "quote"
       const newBurger = {
-        burger_name: document.getElementById('ca').value.trim(),
-        devoured: document.getElementById('devoured').checked,
+        burger_name: document.getElementById("bu").value.trim(),
+        devoured: 0,
       };
 
       // Send POST request to create a new quote
-      fetch('/api/burgers', {
-        method: 'POST',
+      fetch("/api/burgers", {
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
 
         // make sure to serialize the JSON body
         body: JSON.stringify(newBurger),
       }).then(() => {
         // Empty the form
-        document.getElementById('ca').value = '';
+        document.getElementById("bu").value = "";
 
         // Reload the page so the user can see the new quote
-        console.log('Created a new burger!');
+        console.log("Created a new burger!");
         location.reload();
+      });
+    });
+  }
+
+  // DELETE
+  const deleteBtns = document.querySelectorAll(".delete-burger");
+
+  // Set up the event listener for deleting a cat.
+  if (deleteBtns) {
+    deleteBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        // Grabs the id of the element that goes by the name, "id"
+        const id = e.target.getAttribute("data-id");
+
+        fetch(`/api/burgers/${id}`, {
+          method: "DELETE",
+        }).then((response) => {
+          // Check that the response is all good
+          // Reload the page so the user can see the new state
+          if (response.ok) {
+            console.log(`deleted burger: ${id}`);
+            location.reload("/");
+          } else {
+            alert("something went wrong!");
+          }
+        });
       });
     });
   }
